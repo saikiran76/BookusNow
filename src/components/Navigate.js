908197611bb69ede959695dc87aef9cbaf4c3ad4
kiran76 <ksknew76@gmail.com>
2024-04-21 02:52:43 +0530

    A minor change

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -3,7 +3,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const Navigate = ({heading="Recommendations"}) =>{
     return(
         <div className="rec flex items-center font-inter">
-            <h1 className="text-white font-semibold">{heading}</h1>
+            <h1 className="font-semibold">{heading}</h1>
             <div className="pt-1 pl-2"><FaArrowRightLong /></div>
         </div>
     )
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -14,7 +14,7 @@ const SecondaryContainer = () => {
    
         <div className="md:mt-32 p-0 pl-5 ml-0 relative z-0">
             <div className="mt-0 mr-0 md:-mt-52 pl-16 md:pl-12 z-18 relative z-18 overflow-hidden">
-            <div className="pb-4 pr-0 mr-2"><Navigate/></div>
+            <div className="pb-4 pr-0 mr-2 text-white"><Navigate/></div>
             <Suspense fallback={<Shimmer/>}>
                     <Recs/>
             </Suspense>
